6421296267630f3ddb00909c558451785b813eee
jdmonc <jdmonc@add9e3be-ae3b-4fbb-8aef-3aece78a31f7>
2011-01-03 07:07:57 +0000

    delete old files
    git-svn-id: http://devel2.mwds.ca/svn/talkbox/truck@3 add9e3be-ae3b-4fbb-8aef-3aece78a31f7

--- classes/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-//include("config.inc.php"); // Includes the db and form info.
-session_start();
-session_unset(); // Destroys the session.
-session_destroy();
-
-	header("Location: ../login.php"); // Goes to login page..
-
-?>
