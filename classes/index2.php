0850429ef753a7489f19d2c50b1b02e129a89f66
jdmonc <jdmonc@add9e3be-ae3b-4fbb-8aef-3aece78a31f7>
2011-01-11 08:31:10 +0000

    git-svn-id: http://devel2.mwds.ca/svn/talkbox/truck@10 add9e3be-ae3b-4fbb-8aef-3aece78a31f7

--- classes/index2.php
+++ classes/index2.php
@@ -3,7 +3,7 @@
 include 'functions.php';
  $talkbox= new talkbox();
 
-include 'config.inc.php';
+#include 'config.inc.php';
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
