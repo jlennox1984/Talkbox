5ef364cd892f3c53c13e0606713f1d0a11d247f9
jdmonc <jdmonc@add9e3be-ae3b-4fbb-8aef-3aece78a31f7>
2011-01-30 06:51:57 +0000

    fix phases table
    git-svn-id: http://devel2.mwds.ca/svn/talkbox/trunk@12 add9e3be-ae3b-4fbb-8aef-3aece78a31f7

--- classes/functions.php
+++ classes/functions.php
@@ -1,4 +1,4 @@
-i<?php
+<?php
 require 'config.inc.php';
 
 
@@ -7,32 +7,29 @@ class talkbox{
 
 	function showword(){
 	global  $DBI;
-	$sql="SELECT phases,filename FROM phases";
+	$sql="SELECT phases FROM phases";
 	$result = pg_query($DBI, $sql) or die("Error in query: $query." . pg_last_error($connection));
-	$rows= pg_num_rows($result);
-	pg_close($DBI);
-	echo "<table>";
-			
-	if ($rows > 0){
-		
-	for($i=0; $i<$rows; $i++){	
-		$row=pg_fetch_row($result,$i);
-	
-		print "<tr><td>"; 
-		print $row[0];
-		print "</td><td>SAY IT </td>";
-		}
-		print "</table>|";
-			}else
-			{
-			print "<h2> There are no phases stored</h2>";
-		}
-	
-	}	
-	
+	$cols=3;
+if(pg_num_rows($result)==0){
+     die('No results returned!');
+}
 
+echo "<table>";
+$i = 0;//indexing variable
+
+while($row = pg_fetch_array($result)){
+     if($i%$cols == 0)echo "<tr>\r\n";
+     echo "<td><a href='#' onclick=sayit('{$row['phases']}')>{$row['phases']}</a></td>";
+     if($i%$cols == $cols -1)echo "</tr>\r\n";
+$i++;
+}
+
+if($i%2){echo "<td>&nbsp;</td><td>&nbsp;</td>";}
+echo "</tr></table>";
+}
 	function showpict(){
 	global  $DBI;
+	global $path_picts;
         $sql="SELECT phases,filename FROM phases";
         $result = pg_query($DBI, $sql) or die("Error in query: $query." . pg_last_error($connection));
         $rows= pg_num_rows($result);
@@ -41,14 +38,15 @@ class talkbox{
 	echo "items->$rows";
 	if ($rows > 0){
 		for ($i=0; $i<$rows; $i++){
-		$row=$pg_fetch_row($result,$i);
+		$row=pg_fetch_row($result,$i);
 		
 		
-	//	print "<td><a img src=".$path_picts."/".$row[1]." name=".$row[0]." ></td>";
+		print "<td><a img src='".$path_picts."/".$row[1]."' name='".$row[0]."' alt='".$row[0]."' ></td>";
 			
 		}	
 
-	}	
+	}
+	print "</table>";
 	}
 
 
