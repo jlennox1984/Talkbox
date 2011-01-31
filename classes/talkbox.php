528e11ee79cdc4ccac6923dbd6409c746fdd6d9e
jdmonc <jdmonc@add9e3be-ae3b-4fbb-8aef-3aece78a31f7>
2011-01-31 05:17:08 +0000

    added new panel
    git-svn-id: http://devel2.mwds.ca/svn/talkbox/trunk@14 add9e3be-ae3b-4fbb-8aef-3aece78a31f7

---
+++ classes/talkbox.php
@@ -0,0 +1,35 @@
+<?php
+include "functions.php";
+$tb= new talkbox();
+?>
+<html>
+<head>
+<title> </title>
+
+	<script type="text/javascript" src="../js/talkbox.js"></script>
+
+</head>
+
+<body>
+<h2>
+Phases
+<?php
+$tb->showphases(); 
+?>
+<hr>
+<form name="Talkbox" method="post" action="">
+<input name="tts1"  id="tts1" value="" >
+<input type="button" onclick="loadvoicebox();"  value="TALK">
+<iframe id='voiceframe'/>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
