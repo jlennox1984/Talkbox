3970408fc76edcb3acde36edd72116ebcbcee546
jdmonc <jdmonc@add9e3be-ae3b-4fbb-8aef-3aece78a31f7>
2011-02-04 00:05:40 +0000

    fixed css
    git-svn-id: http://devel2.mwds.ca/svn/talkbox/trunk@24 add9e3be-ae3b-4fbb-8aef-3aece78a31f7

--- classes/talkbox.php
+++ classes/talkbox.php
@@ -5,7 +5,7 @@ $tb= new talkbox();
 <html>
 <head>
 <title> </title>
-
+	<link rel="stylesheet" type="text/css" href="cake/css/cake.generic.css" />
 	<script type="text/javascript" src="../js/talkbox.js"></script>
 
 </head>
@@ -19,7 +19,7 @@ $tb->showphases();
 <hr>
 <form name="Talkbox" method="post" action="">
 <input name="tts1"  id="tts1" value="" >
-<input type="button" onclick="loadvoicebox();"  value="TALK">
+<input type="submit" onclick="loadvoicebox();"  value="TALK">
 <iframe id='voiceframe'/>
 </body>
 
