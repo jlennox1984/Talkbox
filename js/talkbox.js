cde38933078370d32d9248d5d6d169b98d7e04db
(no author) <(no author)@add9e3be-ae3b-4fbb-8aef-3aece78a31f7>
2011-11-19 02:50:56 +0000

    ADDED DATE AND TIME FUNCTIONS AND POPUP FUNCTION FOR  VERBS
    git-svn-id: http://devel2.mwds.ca/svn/talkbox/trunk@57 add9e3be-ae3b-4fbb-8aef-3aece78a31f7

--- js/talkbox.js
+++ js/talkbox.js
@@ -61,6 +61,11 @@ function savephase(hid){
 	
 }
 
+function popitup(url) {
+	newwindow=window.open(url,'name','height=200,width=150');
+	if (window.focus) {newwindow.focus()}
+	return false;
+}
 function runScript(e) {
     if (e.keyCode == 13) {
 	this.loadvoicebox();
