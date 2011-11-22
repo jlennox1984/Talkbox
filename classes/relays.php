b0391ef380679d0ed599914a36b643d3917cf823
(no author) <(no author)@add9e3be-ae3b-4fbb-8aef-3aece78a31f7>
2011-11-22 05:49:38 +0000

    1.FIX AJAX RELAYS IN A SINGLE FILE relays.php FOR METHODS SAVEVOL,SAVPHASE,RECORDON AND RECORDOFF 2.MADE RECORDON AND RECORDOFF
    git-svn-id: http://devel2.mwds.ca/svn/talkbox/trunk@58 add9e3be-ae3b-4fbb-8aef-3aece78a31f7

---
+++ classes/relays.php
@@ -0,0 +1,25 @@
+<?php
+include "functions.php";
+$tb= new talkbox();
+
+// relays for talkbox
+
+$action=$_REQUEST['action'];
+		
+		if($action=='vol'){
+		$VOL=$_REQUEST['vol'];
+		 $tb->updateconfig('vol',$VOL);
+
+		} elseif ($action=='savephase'){
+			$hid=$_REQUEST['hid'];
+			$board=$_REQUEST['board'];
+ 			$tb-> savephase($hid,$board);
+		}  elseif($action=='recordon'){
+			$tb->recordon();
+		}elseif ($action=='recordoff'){
+			$tb->recordoff();
+		}
+
+?>
+
+
