27d376ad730ee96ab79c2b82b8f8de59c065b270
jmoncrieff <jeffrey.moncrieff@yahoo.ca>
2012-08-07 22:14:49 -0700

    Changed config for netbook build

--- classes/config.inc.php
+++ classes/config.inc.php
@@ -7,17 +7,20 @@
 
 $dhost='localhost';
 $username="postgres";
-$pass="";
+$pass="postgres"
+;
+
+
 $password=$pass;
 //$db=mysql_connect($dhost, $username, $pass)or die     ('Error connecting to mysql');
 $dbname='talkbox';
 
-$DBI=pg_connect("dbname=$dbname user=$username  password=$password host=localhost ");
+$DBI=pg_connect("dbname=$dbname user=$username  password=$password host=localhost");
 $db=$DBI;
 $database=$dbname;
 $cfg_level=1;
-$abs_path="/var/www/devel.mwds.ca/web/talkbox2/";
-$pic_path="/var/www/devel.mwds.info/web/talkbox2/pics";
-$TBUrl='http://development.mwds.ca/talkbox2/classes';
-$audiodir="/var/www/devel.mwds.info/web/talkbox2/classes/tts/mp3_tts";
+$abs_path="/var/www/";
+$pic_path="/var/www/pics";
+$TBUrl='http://localhost/classes';
+$audiodir="/var/www/classes/tts/mp3_tts";
 ?>
