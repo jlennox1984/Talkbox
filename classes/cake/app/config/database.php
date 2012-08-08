2b28731d785679a852ead281c00658c36e3c8a6e
jmoncrieff <jeffrey.moncrieff@yahoo.ca>
2012-08-07 22:26:02 -0700

    changed database config

--- classes/cake/app/config/database.php
+++ classes/cake/app/config/database.php
@@ -6,7 +6,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'postgres',
-		'password' => '5373988',
+		'password' => 'postgres',
 		'database' => 'talkbox',
 		'schema' => 'public',
 	);
@@ -15,7 +15,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'postgres',
-		'password' => '5373988',
+		'password' => 'postgres',
 		'database' => '1771',
 		'schema' => 'public',
 	);
